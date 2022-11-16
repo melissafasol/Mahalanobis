98ee02169de551df37529871ad5078640ac6fc48
melissafasol <melissaclairefasol@gmail.com>
2022-11-16 12:22:32 +0000

    quefrency

--- cepstrum.py
+++ cepstrum.py
@@ -8,15 +8,15 @@ def compute_cepstrum(signal, sample_freq):
     frame_size = signal.size
     windowed_signal = np.hamming(frame_size) * signal
     dt = 1/sample_freq
-    freq_vector = np.fft.rfftfreq(frame_size, d=dt)
-    X = np.fft.rfft(windowed_signal)
+    freq_vector = np.fft.fftfreq(frame_size, d=dt)
+    X = np.fft.fft(windowed_signal)
     log_X = np.log(np.abs(X))
-    cepstrum = np.fft.rfft(log_X)
+    cepstrum = np.fft.ifft(log_X)
     df = freq_vector[1] - freq_vector[0]
-    quefrency_vector = np.fft.rfftfreq(log_X.size, df)
+    quefrency_vector = np.fft.fftfreq(log_X.size, df)
     return quefrency_vector, cepstrum
 
-def cepstrum_f0_detection(signal, sample_freq, fmin=82, fmax=640):
+def cepstrum_f0_detection(signal, sample_freq, fmin=2, fmax=30):
     """Returns f0 based on cepstral processing."""
     quefrency_vector, cepstrum = compute_cepstrum(signal, sample_freq)
     # extract peak in cepstrum in valid region
